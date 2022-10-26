2bf5d890174dc0616fad1f7d27ea479211cf6b2c
santigandolfo <santiago.gandolfo@tiendanube.com>
2022-10-25 22:54:18 -0300

    Generated example primereact next.js screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,8 @@
 import './globals.css'
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 
 export default function RootLayout({
   children,
