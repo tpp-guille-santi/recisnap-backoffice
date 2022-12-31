a6433f4d85bdaa806aa75136a96d805263a6f79a
santigandolfo <santiago.gandolfo@tiendanube.com>
2022-12-31 17:18:39 -0300

    Added primereact list

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,10 @@
 /* eslint-disable @next/next/no-head-element */
 import Link from 'next/link';
 import './globals.css';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 
 export default function RootLayout({
   children,
@@ -13,8 +17,8 @@ export default function RootLayout({
             <Link href="/">
               Home
             </Link>
-            <Link href="/notes">
-              Notes
+            <Link href="/instructions">
+              Instructions
             </Link>
           </nav>
           {children}
