f72907f334535d4faa99a2845448061f2fa103a4
Guillermo Czop <guilleczop@gmail.com>
2023-01-04 22:40:15 -0300

    Agregado primer ejemplo de dropdown
    * Se agrega un archivo inicial con las provincias argentinas
    * /Homepage tiene un dropdown que lista las provincias leidas del archivo.

--- app/homepage/page.js
+++ app/homepage/page.js
@@ -1,7 +1,27 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import { Dropdown } from "primereact/dropdown";
+import data from "../../config/argentina.json";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  const [province, setProvince] = useState(data.provincias[0]);
+
+  const printInfo = () => {
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <div>Homepage</div>
+      <Dropdown
+        optionLabel="nombre"
+        value={province}
+        options={data.provincias}
+        placeholder="Select a City"
+        onChange={(e) => setProvince(e.value)}
+      ></Dropdown>
+    </div>
+  );
 };
 
 export default Homepage;
