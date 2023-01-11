414610f2f83bdbb64a7f0110c43c5bd822b409c2
Guillermo Czop <guilleczop@gmail.com>
2023-01-10 23:11:25 -0300

    Agregado tema custom

--- app/layout.js
+++ app/layout.js
@@ -1,25 +1,19 @@
 /* eslint-disable @next/next/no-head-element */
-import Link from 'next/link';
-import './globals.css';
-import 'primereact/resources/themes/saga-blue/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.css';
+import Link from "next/link";
+import "./globals.css";
+import "../styles/recisnap-theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 
-export default function RootLayout({
-  children,
-}) {
+export default function RootLayout({ children }) {
   return (
     <html>
       <body>
         <main>
           <nav>
-            <Link href="/">
-              Home
-            </Link>
-            <Link href="/instructions">
-              Instructions
-            </Link>
+            <Link href="/">Home</Link>
+            <Link href="/instructions">Instructions</Link>
           </nav>
           {children}
         </main>
