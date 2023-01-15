a97e7809abd939590cbc858afe209f442a7f07cb
santigandolfo <santiago.gandolfo@tiendanube.com>
2023-01-15 02:47:03 -0300

    Added Instructions create and update functionality

--- components/dropdownFilter.js
+++ components/dropdownFilter.js
@@ -4,7 +4,9 @@ import { Dropdown } from "primereact/dropdown";
 import { Card } from "primereact/card";
 import data from "../config/argentina.json";
 
+
 const DropdownFilter = (props) => {
+  const [material, setMaterial] = useState(props.defaultMaterial);
   const [province, setProvince] = useState();
   const [departmentList, setDepartmentList] = useState([]);
   const [deparment, setDepartment] = useState();
@@ -12,6 +14,29 @@ const DropdownFilter = (props) => {
   const [municipio, setMunicipio] = useState();
   const [departmentDisabled, setDepartmentDisabled] = useState(true);
   const [municipioDisabled, setMunicipioDisabled] = useState(true);
+
+
+  useEffect(() => {
+    if (props.preloaded) {
+      setMaterial(props.materials.find(val => val.name === props.instruction.material_name));
+      const _provincia = data.provincias.find(provincia => provincia.nombre === props.instruction.provincia)
+      const _departamento = _provincia.departamentos.find(departamento => departamento.nombre === props.instruction.departamento)
+      const _municipio = _departamento.municipios.find(municipio => municipio.nombre === props.instruction.municipio)
+      setProvince(_provincia);
+      setDepartment(_departamento);
+      setMunicipio(_municipio);
+      setDepartmentList([_departamento])
+      setmunicipiosList([_municipio])
+    }
+  }, []);
+
+
+
+
+  const materialSelected = (material) => {
+    setMaterial(material);
+    props.material(material);
+  };
 
   const provinceSelected = (province) => {
     setProvince(province);
@@ -38,30 +63,42 @@ const DropdownFilter = (props) => {
     <Card>
       <Dropdown
         style={{ margin: "5px 5px 5px 5px" }}
+        optionLabel="name"
+        value={material}
+        options={props.materials}
+        placeholder="Seleccione un material"
+        onChange={(e) => materialSelected(e.value)}
+        required
+        autoFocus
+        disabled={props.preloaded}
+      ></Dropdown>
+      <Dropdown
+        style={{ margin: "5px 5px 5px 5px" }}
         optionLabel="nombre"
         value={province}
         options={data.provincias}
         placeholder="Seleccione una provincia"
         onChange={(e) => provinceSelected(e.value)}
+        disabled={props.preloaded}
       ></Dropdown>
       <Dropdown
         style={{ margin: "5px 5px 5px 5px" }}
         optionLabel="nombre"
         value={deparment}
-        options={departmentList}
+        options={departmentList ? departmentList : [deparment]}
         placeholder="Seleccione un departamento"
         onChange={(e) => departmentSelected(e.value)}
-        disabled={departmentDisabled}
+        disabled={props.preloaded || departmentDisabled}
       ></Dropdown>
       <Dropdown
         style={{ margin: "5px 5px 5px 5px" }}
         optionLabel="nombre"
         value={municipio}
-        options={municipiosList}
+        options={municipiosList ? municipiosList : [municipio]}
         placeholder="Seleccione un municipio"
         emptyMessage="El departamento no posee municipios"
         onChange={(e) => municipioSelected(e.value)}
-        disabled={municipioDisabled}
+        disabled={props.preloaded || municipioDisabled}
       ></Dropdown>
     </Card>
   );
