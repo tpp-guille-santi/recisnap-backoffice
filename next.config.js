b3d37968bbdca856ef9d5f275a7971c34757aade
santigandolfo <santiago.gandolfo@tiendanube.com>
2023-01-17 01:33:36 -0300

    Componente para visualizar y editar Markdown

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const removeImports = require('next-remove-imports')({});
+module.exports = removeImports({
   reactStrictMode: true,
   swcMinify: true,
-  experimental:{ appDir: true },
+  experimental: { appDir: true, esmExternals: 'loose' },
   reactStrictMode: false,
-}
-
-module.exports = nextConfig
+});
