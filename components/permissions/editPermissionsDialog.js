bbbbfa0270e486d133d3299b8f72998a6e47eab5
Guillermo Czop <guilleczop@gmail.com>
2023-02-11 10:49:28 -0300

    Cambios al PermissonDialog

--- components/permissions/editPermissionsDialog.js
+++ components/permissions/editPermissionsDialog.js
@@ -1,9 +1,17 @@
-import { React } from 'react';
+'use client';
+import { React, useState } from 'react';
 import TogglableEntry from './togglableEntry';
 import { Dialog } from 'primereact/dialog';
 import { Button } from 'primereact/button';
 
 const EditPermissionsDialog = props => {
+  const [canViewDocument, setCanViewDocument] = useState(false);
+  const [canCreateDocument, setCanCreateDocument] = useState(false);
+  const [canEditDocument, setCanEditDocument] = useState(false);
+  const [canBlockDocument, setCanBlockDocument] = useState(false);
+  const [canEditPermissions, setCanEditPermissions] = useState(false);
+  const [canViewUsers, setCanViewUsers] = useState();
+
   return (
     <div>
       <Dialog
@@ -16,41 +24,58 @@ const EditPermissionsDialog = props => {
             <TogglableEntry
               label="Ver documento"
               initialState={props.permissions.includes('view_pages')}
+              changeValue={setCanViewDocument}
             ></TogglableEntry>
           </div>
           <div>
             <TogglableEntry
               label="Nuevo documento"
               initialState={props.permissions.includes('create_page')}
+              changeValue={setCanCreateDocument}
             ></TogglableEntry>
           </div>
           <div>
             <TogglableEntry
               label="Blockear documento"
               initialState={props.permissions.includes('block_page')}
+              changeValue={setCanBlockDocument}
             ></TogglableEntry>
           </div>
           <div>
             <TogglableEntry
               label="Editar documento"
               initialState={props.permissions.includes('edit_page')}
+              changeValue={setCanEditDocument}
             ></TogglableEntry>
           </div>
           <div>
             <TogglableEntry
               label="Dar permisos"
               initialState={props.permissions.includes('grant_permissions')}
+              changeValue={setCanEditPermissions}
             ></TogglableEntry>
           </div>
           <div>
             <TogglableEntry
               label="Ver usuario"
               initialState={props.permissions.includes('view_users')}
+              changeValue={setCanViewUsers}
             ></TogglableEntry>
           </div>
         </div>
         <div>
-          <Button></Button>
+          <Button
+            onClick={() => {
+              console.log({
+                viewDoc: canViewDocument,
+                block: canBlockDocument,
+                newDocs: canCreateDocument,
+                editDoc: canEditDocument,
+                users: canViewUsers,
+                permi: canEditPermissions
+              });
+            }}
+          ></Button>
         </div>
       </Dialog>
     </div>
--- components/permissions/togglableEntry.js
+++ components/permissions/togglableEntry.js
@@ -1,14 +1,21 @@
+'use client';
 import { React, useState } from 'react';
 import { ToggleButton } from 'primereact/togglebutton';
 
 const TogglableEntry = props => {
   const [checked, setChecked] = useState(props.initialState);
+
+  const onChangeValue = value => {
+    setChecked(value);
+    props.changeValue(value);
+  };
+
   return (
     <div>
       <label>{props.label}</label>
       <ToggleButton
         checked={checked}
-        onChange={e => setChecked(e.value)}
+        onChange={e => onChangeValue(e.value)}
       ></ToggleButton>
     </div>
   );
